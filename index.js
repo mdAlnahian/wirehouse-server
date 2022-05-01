7e0de8696d9ded2f76471419a2b7a3421e71f238
mdAlnahian <muhammedalnahian@gmail.com>
2022-05-01 17:47:18 +0600

    delete method applied

--- index.js
+++ index.js
@@ -49,6 +49,14 @@ async function run(){
             res.send(result)
         })
 
+        //delete an item 
+        app.delete('/item/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query ={_id:ObjectId(id)};
+            const result = await itemCollection.deleteOne(query);
+            res.send(result);
+        });
+
     }
     finally{
 
